793f6f0cb045bddc79e9c830d31ff54a049d5bb3
Ted <shinsuengju1@gmail.com>
2023-06-20 01:07:16 +0900

    Day9

--- Day9/02.Morse code.js
+++ Day9/02.Morse code.js
@@ -43,6 +43,44 @@ function solution(letter) {
   return answer;
 }
 
+// JS 기능 활용
+function solution(letter) {
+  const morse = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+  };
+  let answer = '';
+  const splittedLetter = letter.split(' ');
+  for (let i = 0; i < splittedLetter.length; i++) {
+    answer += morse[splittedLetter[i]];
+  }
+  return answer;
+}
+
 // 나의 풀이
 function solution(letter) {
   const morse = {
