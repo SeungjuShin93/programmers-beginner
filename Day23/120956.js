598e32a94e687430aa52fe49038aa42546849c51
Ted <shinsuengju1@gmail.com>
2023-07-21 12:59:11 +0900

    120956 without 정규식

--- Day23/120956.js
+++ Day23/120956.js
@@ -11,3 +11,18 @@ function solution(babbling) {
   return cnt;
 }
 console.log(solution(['ayaye', 'uuuma', 'ye', 'yemawoo', 'ayaa']));
+
+// 정규식 없는 풀이 by 계란찜
+function solution(babbling) {
+  const one = ['aya', 'ye', 'woo', 'ma'];
+  let count = 0;
+  for (let i = 0; i < babbling.length; i++) {
+    for (let j = 0; j < one.length; j++) {
+      babbling[i] = babbling[i].replace(one[j], '-');
+    }
+  }
+  for (let k = 0; k < babbling.length; k++) {
+    if (babbling[k].split('-').join('') === '') count++;
+  }
+  return count;
+}
